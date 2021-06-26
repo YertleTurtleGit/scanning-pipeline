49ba40349ad1633edbee7cd069338e68b6f3f47d
YertleTurtleGit <github@galzone.de>
2021-06-26 14:06:00 +0200

    fixed point size

--- point-cloud-helper.js
+++ point-cloud-helper.js
@@ -129,7 +129,7 @@ class PointCloudHelper {
       this.geometry = new THREE.BufferGeometry();
 
       this.material = new THREE.PointsMaterial({
-         size: 0.001,
+         size: 1,
          vertexColors: true,
       });
       this.pointCloud = new THREE.Points(this.geometry, this.material);
