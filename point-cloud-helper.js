a5dd38c319ea636520d380242afba3bb35fab794
YertleTurtleGit <github@galzone.de>
2021-07-02 11:41:09 +0200

    changed point cloud camera settings

--- point-cloud-helper.js
+++ point-cloud-helper.js
@@ -2,278 +2,282 @@
 "use strict";
 
 class PointCloudHelper {
-  /** @private */
-  static renderId = 0;
+   /** @private */
+   static renderId = 0;
 
-  /**
-   * @public
-   * @param {HTMLImageElement} depthMapImage
-   * @param {HTMLCanvasElement} renderCanvas
-   * @param {HTMLImageElement} textureImage
-   * @param {boolean} cancelIfNewJobSpawned
-   * @returns {Promise<number[]>}
-   */
-  static async calculatePointCloud(
-    depthMapImage,
-    renderCanvas,
-    depthFactor = 0.15,
-    cancelIfNewJobSpawned = false,
-    textureImage = depthMapImage
-  ) {
-    const pointCloudHelper = new PointCloudHelper(
+   /**
+    * @public
+    * @param {HTMLImageElement} depthMapImage
+    * @param {HTMLCanvasElement} renderCanvas
+    * @param {HTMLImageElement} textureImage
+    * @param {boolean} cancelIfNewJobSpawned
+    * @returns {Promise<number[]>}
+    */
+   static async calculatePointCloud(
+      depthMapImage,
       renderCanvas,
-      cancelIfNewJobSpawned
-    );
+      depthFactor = 0.15,
+      cancelIfNewJobSpawned = false,
+      textureImage = depthMapImage
+   ) {
+      const pointCloudHelper = new PointCloudHelper(
+         renderCanvas,
+         cancelIfNewJobSpawned
+      );
 
-    return await new Promise((resolve) => {
-      setTimeout(async () => {
-        if (pointCloudHelper.isRenderObsolete()) return;
-        await pointCloudHelper.renderingContext.initialize();
-        if (pointCloudHelper.isRenderObsolete()) return;
+      return await new Promise((resolve) => {
+         setTimeout(async () => {
+            if (pointCloudHelper.isRenderObsolete()) return;
+            await pointCloudHelper.renderingContext.initialize();
+            if (pointCloudHelper.isRenderObsolete()) return;
 
-        const dataCanvas = document.createElement("canvas");
-        dataCanvas.width = depthMapImage.naturalWidth;
-        dataCanvas.height = depthMapImage.naturalHeight;
-        const dataContext = dataCanvas.getContext("2d");
+            const dataCanvas = document.createElement("canvas");
+            dataCanvas.width = depthMapImage.naturalWidth;
+            dataCanvas.height = depthMapImage.naturalHeight;
+            const dataContext = dataCanvas.getContext("2d");
 
-        dataContext.drawImage(depthMapImage, 0, 0);
+            dataContext.drawImage(depthMapImage, 0, 0);
 
-        if (pointCloudHelper.isRenderObsolete()) return;
+            if (pointCloudHelper.isRenderObsolete()) return;
 
-        const imageData = dataContext.getImageData(
-          0,
-          0,
-          dataCanvas.width,
-          dataCanvas.height
-        ).data;
+            const imageData = dataContext.getImageData(
+               0,
+               0,
+               dataCanvas.width,
+               dataCanvas.height
+            ).data;
 
-        dataContext.drawImage(textureImage, 0, 0);
+            dataContext.drawImage(textureImage, 0, 0);
 
-        if (pointCloudHelper.isRenderObsolete()) return;
+            if (pointCloudHelper.isRenderObsolete()) return;
 
-        const textureData = dataContext.getImageData(
-          0,
-          0,
-          dataCanvas.width,
-          dataCanvas.height
-        ).data;
+            const textureData = dataContext.getImageData(
+               0,
+               0,
+               dataCanvas.width,
+               dataCanvas.height
+            ).data;
 
-        const vertexColors = [];
-        const vertices = [];
+            const vertexColors = [];
+            const vertices = [];
 
-        const maxDimension = Math.max(dataCanvas.width, dataCanvas.height);
-        /*const aspectWidth = dataCanvas.width / dataCanvas.height;
+            const maxDimension = Math.max(dataCanvas.width, dataCanvas.height);
+            /*const aspectWidth = dataCanvas.width / dataCanvas.height;
         const aspectHeight = dataCanvas.height / dataCanvas.width;*/
 
-        for (let x = 0; x < dataCanvas.width; x++) {
-          for (let y = 0; y < dataCanvas.height; y++) {
-            const index = (y * dataCanvas.width + x) * 4;
-            vertices.push(
-              (x / maxDimension - 0.5) * 100,
-              (1 - y / maxDimension - 0.75) * 100,
-              (imageData[index] / 255) * 100 * depthFactor
+            for (let x = 0; x < dataCanvas.width; x++) {
+               for (let y = 0; y < dataCanvas.height; y++) {
+                  const index = (y * dataCanvas.width + x) * 4;
+                  vertices.push(
+                     (x / maxDimension - 0.5) * 100,
+                     (1 - y / maxDimension - 0.75) * 100,
+                     (imageData[index] / 255) * 100 * depthFactor
+                  );
+                  vertexColors.push(
+                     textureData[index + 0] / 255,
+                     textureData[index + 1] / 255,
+                     textureData[index + 2] / 255
+                  );
+               }
+               if (pointCloudHelper.isRenderObsolete()) return;
+            }
+
+            resolve(vertices);
+
+            pointCloudHelper.renderingContext.geometry.setAttribute(
+               "position",
+               new THREE.Float32BufferAttribute(vertices, 3)
             );
-            vertexColors.push(
-              textureData[index + 0] / 255,
-              textureData[index + 1] / 255,
-              textureData[index + 2] / 255
+            pointCloudHelper.renderingContext.geometry.setAttribute(
+               "color",
+               new THREE.Float32BufferAttribute(vertexColors, 3)
             );
-          }
-          if (pointCloudHelper.isRenderObsolete()) return;
-        }
 
-        resolve(vertices);
+            if (pointCloudHelper.isRenderObsolete()) return;
 
-        pointCloudHelper.renderingContext.geometry.setAttribute(
-          "position",
-          new THREE.Float32BufferAttribute(vertices, 3)
-        );
-        pointCloudHelper.renderingContext.geometry.setAttribute(
-          "color",
-          new THREE.Float32BufferAttribute(vertexColors, 3)
-        );
+            pointCloudHelper.renderingContext.geometry.attributes.position.needsUpdate = true;
+            pointCloudHelper.renderingContext.geometry.attributes.color.needsUpdate = true;
 
-        if (pointCloudHelper.isRenderObsolete()) return;
+            pointCloudHelper.renderingContext.render();
 
-        pointCloudHelper.renderingContext.geometry.attributes.position.needsUpdate = true;
-        pointCloudHelper.renderingContext.geometry.attributes.color.needsUpdate = true;
+            pointCloudHelper.renderingContext.handleResize();
+         }, 100);
+      });
+   }
 
-        pointCloudHelper.renderingContext.render();
+   /**
+    * @public
+    * @param {HTMLCanvasElement} canvas
+    */
+   static clearCanvas(canvas) {
+      const pointCloudHelperRenderingContext =
+         PointCloudHelperRenderingContext.getInstance(canvas);
 
-        pointCloudHelper.renderingContext.handleResize();
-      }, 100);
-    });
-  }
+      pointCloudHelperRenderingContext.geometry.setAttribute(
+         "position",
+         new THREE.Float32BufferAttribute([], 3)
+      );
+      pointCloudHelperRenderingContext.geometry.setAttribute(
+         "color",
+         new THREE.Float32BufferAttribute([], 3)
+      );
 
-  /**
-   * @public
-   * @param {HTMLCanvasElement} canvas
-   */
-  static clearCanvas(canvas) {
-    const pointCloudHelperRenderingContext =
-      PointCloudHelperRenderingContext.getInstance(canvas);
+      pointCloudHelperRenderingContext.geometry.attributes.position.needsUpdate = true;
+      pointCloudHelperRenderingContext.geometry.attributes.color.needsUpdate = true;
 
-    pointCloudHelperRenderingContext.geometry.setAttribute(
-      "position",
-      new THREE.Float32BufferAttribute([], 3)
-    );
-    pointCloudHelperRenderingContext.geometry.setAttribute(
-      "color",
-      new THREE.Float32BufferAttribute([], 3)
-    );
+      pointCloudHelperRenderingContext.render();
+   }
 
-    pointCloudHelperRenderingContext.geometry.attributes.position.needsUpdate = true;
-    pointCloudHelperRenderingContext.geometry.attributes.color.needsUpdate = true;
+   /**
+    * @private
+    * @param {HTMLCanvasElement} renderCanvas
+    * @param {boolean} cancelIfNewJobSpawned
+    */
+   constructor(renderCanvas, cancelIfNewJobSpawned) {
+      PointCloudHelper.renderId++;
 
-    pointCloudHelperRenderingContext.render();
-  }
+      this.renderId = PointCloudHelper.renderId;
+      this.cancelIfNewJobSpawned = cancelIfNewJobSpawned;
 
-  /**
-   * @private
-   * @param {HTMLCanvasElement} renderCanvas
-   * @param {boolean} cancelIfNewJobSpawned
-   */
-  constructor(renderCanvas, cancelIfNewJobSpawned) {
-    PointCloudHelper.renderId++;
+      this.renderingContext =
+         PointCloudHelperRenderingContext.getInstance(renderCanvas);
+   }
 
-    this.renderId = PointCloudHelper.renderId;
-    this.cancelIfNewJobSpawned = cancelIfNewJobSpawned;
-
-    this.renderingContext =
-      PointCloudHelperRenderingContext.getInstance(renderCanvas);
-  }
-
-  /**
-   * @private
-   * @returns {boolean}
-   */
-  isRenderObsolete() {
-    return (
-      this.cancelIfNewJobSpawned && this.renderId < PointCloudHelper.renderId
-    );
-  }
+   /**
+    * @private
+    * @returns {boolean}
+    */
+   isRenderObsolete() {
+      return (
+         this.cancelIfNewJobSpawned && this.renderId < PointCloudHelper.renderId
+      );
+   }
 }
 
 class PointCloudHelperRenderingContext {
-  /** @private @constant */
-  static MAX_INSTANCES = 8;
+   /** @private @constant */
+   static MAX_INSTANCES = 8;
 
-  /**
-   * @private
-   * @type {PointCloudHelperRenderingContext[]}
-   */
-  static instances = [];
+   /**
+    * @private
+    * @type {PointCloudHelperRenderingContext[]}
+    */
+   static instances = [];
 
-  /**
-   * @public
-   * @param {HTMLCanvasElement} renderCanvas
-   * @returns {PointCloudHelperRenderingContext}
-   */
-  static getInstance(renderCanvas) {
-    for (
-      let i = 0;
-      i < PointCloudHelperRenderingContext.instances.length;
-      i++
-    ) {
-      const testInstance = PointCloudHelperRenderingContext.instances[i];
-      if (testInstance.renderCanvas === renderCanvas) {
-        const instance = testInstance;
-        return instance;
+   /**
+    * @public
+    * @param {HTMLCanvasElement} renderCanvas
+    * @returns {PointCloudHelperRenderingContext}
+    */
+   static getInstance(renderCanvas) {
+      for (
+         let i = 0;
+         i < PointCloudHelperRenderingContext.instances.length;
+         i++
+      ) {
+         const testInstance = PointCloudHelperRenderingContext.instances[i];
+         if (testInstance.renderCanvas === renderCanvas) {
+            const instance = testInstance;
+            return instance;
+         }
       }
-    }
 
-    const instance = new PointCloudHelperRenderingContext(renderCanvas);
-    return instance;
-  }
+      const instance = new PointCloudHelperRenderingContext(renderCanvas);
+      return instance;
+   }
 
-  /**
-   * @private
-   * @param {HTMLCanvasElement} renderCanvas
-   */
-  constructor(renderCanvas) {
-    this.initialized = false;
-    this.renderCanvas = renderCanvas;
+   /**
+    * @private
+    * @param {HTMLCanvasElement} renderCanvas
+    */
+   constructor(renderCanvas) {
+      this.initialized = false;
+      this.renderCanvas = renderCanvas;
 
-    this.renderer = new THREE.WebGLRenderer({
-      canvas: renderCanvas,
-      alpha: true,
-      antialias: true,
-    });
-    this.camera = new THREE.PerspectiveCamera(
-      25,
-      renderCanvas.width / renderCanvas.height
-    );
-    this.controls = new THREE.OrbitControls(this.camera, this.renderCanvas);
-    this.scene = new THREE.Scene();
-    this.geometry = new THREE.BufferGeometry();
-    this.material = new THREE.PointsMaterial({
-      size: 2,
-      vertexColors: true,
-    });
-    this.pointCloud = new THREE.Points(this.geometry, this.material);
-
-    PointCloudHelperRenderingContext.instances.push(this);
-
-    if (
-      PointCloudHelperRenderingContext.instances.length >
-      PointCloudHelperRenderingContext.MAX_INSTANCES
-    ) {
-      console.warn(
-        "PointCloudHelperRenderingContext exceeded maximum render canvas instance count. The last instance gets deleted."
-      );
-      PointCloudHelperRenderingContext.instances.shift();
-    }
-  }
-
-  async render() {
-    await this.initialize();
-    this.renderer.render(this.scene, this.camera);
-  }
-
-  /**
-   * @public
-   */
-  async initialize() {
-    if (this.initialized) {
-      return;
-    }
-    await new Promise((resolve) => {
-      setTimeout(() => {
-        this.scene.add(this.pointCloud);
-
-        this.camera.position.z = 75;
-        this.camera.position.y = -100;
-
-        this.controls.target = new THREE.Vector3(0, 0, 0);
-
-        this.initialized = true;
-        resolve();
-
-        this.controls.addEventListener("change", () => {
-          this.render();
-        });
-        window.addEventListener("resize", this.handleResize.bind(this));
-
-        this.controls.update();
-        this.handleResize();
+      this.renderer = new THREE.WebGLRenderer({
+         canvas: renderCanvas,
+         alpha: true,
+         antialias: true,
       });
-    });
-  }
+      this.camera = new THREE.PerspectiveCamera(
+         50,
+         renderCanvas.width / renderCanvas.height,
+         0.01,
+         1000
+      );
+      this.controls = new THREE.OrbitControls(this.camera, this.renderCanvas);
+      this.scene = new THREE.Scene();
+      this.geometry = new THREE.BufferGeometry();
+      this.material = new THREE.PointsMaterial({
+         size: 2,
+         vertexColors: true,
+      });
+      this.pointCloud = new THREE.Points(this.geometry, this.material);
 
-  /**
-   * @public
-   */
-  handleResize() {
-    const width = this.renderCanvas.clientWidth;
-    const height = this.renderCanvas.clientHeight;
-    const needResize =
-      this.renderCanvas.width !== width || this.renderCanvas.height !== height;
-    if (needResize) {
-      this.renderer.setSize(width, height);
+      PointCloudHelperRenderingContext.instances.push(this);
 
-      this.camera.aspect = this.renderCanvas.width / this.renderCanvas.height;
-      this.camera.updateProjectionMatrix();
-      this.render();
-    }
-  }
+      if (
+         PointCloudHelperRenderingContext.instances.length >
+         PointCloudHelperRenderingContext.MAX_INSTANCES
+      ) {
+         console.warn(
+            "PointCloudHelperRenderingContext exceeded maximum render canvas instance count. The last instance gets deleted."
+         );
+         PointCloudHelperRenderingContext.instances.shift();
+      }
+   }
+
+   async render() {
+      await this.initialize();
+      this.renderer.render(this.scene, this.camera);
+   }
+
+   /**
+    * @public
+    */
+   async initialize() {
+      if (this.initialized) {
+         return;
+      }
+      await new Promise((resolve) => {
+         setTimeout(() => {
+            this.scene.add(this.pointCloud);
+
+            this.camera.position.z = 75;
+            this.camera.position.y = -100;
+
+            this.controls.target = new THREE.Vector3(0, 0, 0);
+
+            this.initialized = true;
+            resolve();
+
+            this.controls.addEventListener("change", () => {
+               this.render();
+            });
+            window.addEventListener("resize", this.handleResize.bind(this));
+
+            this.controls.update();
+            this.handleResize();
+         });
+      });
+   }
+
+   /**
+    * @public
+    */
+   handleResize() {
+      const width = this.renderCanvas.clientWidth;
+      const height = this.renderCanvas.clientHeight;
+      const needResize =
+         this.renderCanvas.width !== width ||
+         this.renderCanvas.height !== height;
+      if (needResize) {
+         this.renderer.setSize(width, height);
+
+         this.camera.aspect =
+            this.renderCanvas.width / this.renderCanvas.height;
+         this.camera.updateProjectionMatrix();
+         this.render();
+      }
+   }
 }
