81930bb66cb4612524462efc45a6c4f880e1827c
YertleTurtleGit <github@galzone.de>
2021-10-14 15:09:16 +0200

    easy teasting

--- src/normal-map-helper.js
+++ src/normal-map-helper.js
@@ -41,11 +41,6 @@ class NormalMapHelper {
    ) {
       const maskThreshold = maskThresholdPercent / 100;
 
-      const dimensions = {
-         width: lightImage_000.naturalWidth * (resolutionPercent / 100),
-         height: lightImage_000.naturalHeight * (resolutionPercent / 100),
-      };
-
       const normalMapHelper = new NormalMapHelper();
 
       return new Promise((resolve) => {
@@ -54,13 +49,234 @@ class NormalMapHelper {
                lightImage_000.naturalWidth < 1 ||
                lightImage_045.naturalWidth < 1 ||
                lightImage_090.naturalWidth < 1 ||
-               lightImage_135.naturalWidth < 1 ||
                lightImage_180.naturalWidth < 1 ||
                lightImage_225.naturalWidth < 1 ||
                lightImage_270.naturalWidth < 1 ||
                lightImage_315.naturalWidth < 1
             )
                return;
+
+            if (normalMapHelper.isRenderObsolete()) return;
+
+            const normalMapShader = new GLSL.Shader({
+               width: lightImage_000.naturalWidth * (resolutionPercent / 100),
+               height: lightImage_000.naturalHeight * (resolutionPercent / 100),
+            });
+
+            if (normalMapHelper.isRenderObsolete()) return;
+
+            normalMapShader.bind();
+
+            const lightLuminances = [
+               new GLSL.Image(lightImage_000).getPixelColor().getLuminance(),
+               new GLSL.Image(lightImage_045).getPixelColor().getLuminance(),
+               new GLSL.Image(lightImage_090).getPixelColor().getLuminance(),
+               new GLSL.Image(lightImage_135).getPixelColor().getLuminance(),
+               new GLSL.Image(lightImage_180).getPixelColor().getLuminance(),
+               new GLSL.Image(lightImage_225).getPixelColor().getLuminance(),
+               new GLSL.Image(lightImage_270).getPixelColor().getLuminance(),
+               new GLSL.Image(lightImage_315).getPixelColor().getLuminance(),
+            ];
+
+            const all = new GLSL.Float(0).maximum(...lightLuminances);
+
+            let mask = new GLSL.Float(1);
+
+            if (
+               lightImage_NONE &&
+               Math.min(
+                  lightImage_NONE.naturalWidth,
+                  lightImage_NONE.naturalHeight
+               ) > 0
+            ) {
+               const lightLuminance_NONE = new GLSL.Image(lightImage_NONE)
+                  .getPixelColor()
+                  .getLuminance();
+
+               for (let i = 0; i < lightLuminances.length; i++) {
+                  lightLuminances[i] =
+                     lightLuminances[i].subtractFloat(lightLuminance_NONE);
+               }
+
+               mask = all
+                  .subtractFloat(lightLuminance_NONE)
+                  .step(new GLSL.Float(maskThreshold));
+            }
+
+            for (let i = 0; i < lightLuminances.length; i++) {
+               lightLuminances[i] = lightLuminances[i].divideFloat(all);
+            }
+
+            /**
+             * @param {GLSL.Float} originLuminance
+             * @param {GLSL.Float} orthogonalLuminance
+             * @param {GLSL.Float} oppositeLuminance
+             * @param {number} originAzimuthalAngleDeg
+             * @param {number} orthogonalAzimuthalAngleDeg
+             * @param {number} oppositeAzimuthalAngleDeg
+             * @returns {GLSL.Vector3}
+             */
+            function getAnisotropicNormalVector(
+               originLuminance,
+               orthogonalLuminance,
+               oppositeLuminance,
+
+               originAzimuthalAngleDeg,
+               orthogonalAzimuthalAngleDeg,
+               oppositeAzimuthalAngleDeg
+            ) {
+               if (normalMapHelper.isRenderObsolete()) return;
+
+               /**
+                * @param {number} azimuthalAngleDeg
+                * @param {number} polarAngleDeg
+                * @returns {GLSL.Vector3}
+                */
+               function getLightDirectionVector(
+                  azimuthalAngleDeg,
+                  polarAngleDeg
+               ) {
+                  const polar = new GLSL.Float(polarAngleDeg).radians();
+                  const azimuthal = new GLSL.Float(azimuthalAngleDeg).radians();
+
+                  return new GLSL.Vector3([
+                     polar.sin().multiplyFloat(azimuthal.cos()),
+                     polar.sin().multiplyFloat(azimuthal.sin()),
+                     polar.cos(),
+                  ]).normalize();
+               }
+
+               const originLightDirection = getLightDirectionVector(
+                  originAzimuthalAngleDeg,
+                  lightPolarAngleDeg
+               );
+               const orthogonalLightDirection = getLightDirectionVector(
+                  orthogonalAzimuthalAngleDeg,
+                  lightPolarAngleDeg
+               );
+               const oppositeLightDirection = getLightDirectionVector(
+                  oppositeAzimuthalAngleDeg,
+                  lightPolarAngleDeg
+               );
+
+               const lightMatrix = new GLSL.Matrix3([
+                  [
+                     originLightDirection.channel(0),
+                     originLightDirection.channel(1),
+                     originLightDirection.channel(2),
+                  ],
+                  [
+                     orthogonalLightDirection.channel(0),
+                     orthogonalLightDirection.channel(1),
+                     orthogonalLightDirection.channel(2),
+                  ],
+                  [
+                     oppositeLightDirection.channel(0),
+                     oppositeLightDirection.channel(1),
+                     oppositeLightDirection.channel(2),
+                  ],
+               ]).inverse();
+
+               const reflection = new GLSL.Vector3([
+                  originLuminance,
+                  orthogonalLuminance,
+                  oppositeLuminance,
+               ]);
+
+               return lightMatrix
+                  .multiplyVector3(reflection)
+                  .normalize()
+                  .addFloat(new GLSL.Float(1))
+                  .divideFloat(new GLSL.Float(2));
+            }
+
+            /** @type {number[][]} */
+            const anisotropicCombinations = [
+               [180, 270, 0],
+               [180, 90, 0],
+               [90, 180, 270],
+               [90, 0, 270],
+               [225, 315, 45],
+               [225, 135, 45],
+               [315, 45, 135],
+               [315, 225, 135],
+            ];
+
+            /** @type {GLSL.Vector3[]} */
+            const normalVectors = [];
+
+            anisotropicCombinations.forEach((combination) => {
+               /**
+                * @param {number} azimuthalAngle
+                * @returns {GLSL.Float}
+                */
+               function getLightLuminance(azimuthalAngle) {
+                  const lightAzimuthalAngles = [
+                     0, 45, 90, 135, 180, 225, 270, 315,
+                  ];
+                  const id = lightAzimuthalAngles.findIndex((value) => {
+                     return value === azimuthalAngle;
+                  });
+
+                  return lightLuminances[id];
+               }
+
+               normalVectors.push(
+                  getAnisotropicNormalVector(
+                     getLightLuminance(combination[0]),
+                     getLightLuminance(combination[1]),
+                     getLightLuminance(combination[2]),
+                     combination[0],
+                     combination[1],
+                     combination[2]
+                  )
+               );
+            });
+
+            let normalVector = new GLSL.Vector3([
+               new GLSL.Float(0),
+               new GLSL.Float(0),
+               new GLSL.Float(0),
+            ])
+               .addVector3(...normalVectors)
+               .divideFloat(new GLSL.Float(normalVectors.length))
+               .normalize()
+               .multiplyFloat(mask);
+
+            if (normalMapHelper.isRenderObsolete()) return;
+
+            if (cameraVerticalShift) {
+               // TODO: use cameraVerticalShift
+               /*const cameraAngle = Math.atan(
+                  1 / Math.tan(lightPolarAngleDeg * (Math.PI / 180))
+               );
+
+               const zero = new GLSL.Float(0);
+               const one = new GLSL.Float(1);
+               const sine = new GLSL.Float(Math.sin(cameraAngle));
+               const cosine = new GLSL.Float(Math.cos(cameraAngle));
+
+               const rotationMatrix = new GLSL.Matrix3([
+                  [one, zero, zero],
+                  [zero, cosine, sine],
+                  [zero, sine.multiplyFloat(new GLSL.Float(-1)), cosine],
+               ]);
+
+               normalVector = rotationMatrix.multiplyVector3(normalVector);*/
+            }
+
+            // TODO: fix alpha
+            const alpha = normalVector
+               .channel(0)
+               .minimum(normalVector.channel(1), normalVector.channel(2))
+               .multiplyFloat(new GLSL.Float(99999))
+               .minimum(new GLSL.Float(1));
+
+            const normalMapRendering = GLSL.render(
+               normalVector.getVector4(alpha)
+            );
+
+            if (normalMapHelper.isRenderObsolete()) return;
 
             const normalMap = await normalMapRendering.getJsImage();
 
@@ -69,6 +285,8 @@ class NormalMapHelper {
             if (uiImageElement && normalMap) {
                uiImageElement.src = normalMap.src;
             }
+
+            normalMapShader.purge();
          }, 100);
       });
    }
@@ -172,15 +390,6 @@ class NormalMapHelper {
             resolve(normalMap);
          });
       });
-   }
-
-   static getPhotometricStereoShader(dimensions) {
-      if (!NormalMapHelper.photometricStereoShader) {
-         NormalMapHelper.photometricStereoShader = new PhotometricStereoShader(
-            dimensions
-         );
-      }
-      return NormalMapHelper.photometricStereoShader;
    }
 
    /**
@@ -302,276 +511,5 @@ class NormalMapHelper {
       });
    }
 }
+
 NormalMapHelper.renderId = 0;
-NormalMapHelper.photometricStereoShader = undefined;
-
-class NormalMapShader {
-   /**
-    * @abstract
-    * @public
-    * @param {{width:number,height:number}} dimensions
-    */
-   constructor(dimensions) {
-      this.dimensions = dimensions;
-      this.initialized = false;
-   }
-}
-
-class PhotometricStereoShader extends NormalMapShader {
-   /**
-    * @public
-    * @param {{width:number,height:number}} dimensions
-    */
-   constructor(dimensions) {
-      super(dimensions);
-      this.generate();
-   }
-
-   /**
-    * @public
-    * @param {HTMLImageElement[]} lightImages
-    */
-   setLightImages(lightImages) {
-      this.photometricStereoLightImageUniforms.forEach((uniform, index) => {
-         uniform.setValue(lightImages[index]);
-      });
-   }
-
-   /**
-    * @public
-    * @returns {Promise<HTMLImageElement>}
-    */
-   async render() {
-      while (!this.initialized) {
-         await new Promise((resolve) => {
-            setTimeout(resolve, 1000);
-         });
-      }
-      this.shader.bind();
-      const rendering = GLSL.render(this.outVariable);
-      this.shader.unbind();
-      return rendering.getJsImage();
-   }
-
-   /**
-    * @private
-    */
-   generate() {
-      this.initialized = false;
-
-      this.shader = new GLSL.Shader(this.dimensions);
-      this.shader.bind();
-
-      this.photometricStereoLightImageUniforms = [
-         new GLSL.Uniform.Image(),
-         new GLSL.Uniform.Image(),
-         new GLSL.Uniform.Image(),
-         new GLSL.Uniform.Image(),
-         new GLSL.Uniform.Image(),
-         new GLSL.Uniform.Image(),
-         new GLSL.Uniform.Image(),
-         new GLSL.Uniform.Image(),
-         new GLSL.Uniform.Image(),
-      ];
-
-      this.lightPolarAngleDegUniform = new GLSL.Uniform.Float();
-      const lightPolarAngleDeg = this.lightPolarAngleDegUniform.getValue();
-
-      const lightLuminances = [];
-      this.photometricStereoLightImageUniforms.forEach((uniform) => {
-         lightLuminances.push(
-            uniform.getValue().getPixelColor().getLuminance()
-         );
-      });
-
-      const all = new GLSL.Float(0).maximum(...lightLuminances);
-
-      let mask = new GLSL.Float(1);
-
-      // TODO Find solution.
-      /*if (
-         lightImage_NONE &&
-         Math.min(lightImage_NONE.naturalWidth, lightImage_NONE.naturalHeight) >
-            0
-      ) {
-         const lightLuminance_NONE = new GLSL.Image(lightImage_NONE)
-            .getPixelColor()
-            .getLuminance();
-
-         for (let i = 0; i < lightLuminances.length; i++) {
-            lightLuminances[i] =
-               lightLuminances[i].subtractFloat(lightLuminance_NONE);
-         }
-
-         mask = all
-            .subtractFloat(lightLuminance_NONE)
-            .step(new GLSL.Float(maskThreshold));
-      }*/
-
-      for (let i = 0; i < lightLuminances.length; i++) {
-         lightLuminances[i] = lightLuminances[i].divideFloat(all);
-      }
-
-      /**
-       * @param {GLSL.Float} originLuminance
-       * @param {GLSL.Float} orthogonalLuminance
-       * @param {GLSL.Float} oppositeLuminance
-       * @param {number} originAzimuthalAngleDeg
-       * @param {number} orthogonalAzimuthalAngleDeg
-       * @param {number} oppositeAzimuthalAngleDeg
-       * @returns {GLSL.Vector3}
-       */
-      function getAnisotropicNormalVector(
-         originLuminance,
-         orthogonalLuminance,
-         oppositeLuminance,
-
-         originAzimuthalAngleDeg,
-         orthogonalAzimuthalAngleDeg,
-         oppositeAzimuthalAngleDeg
-      ) {
-         /**
-          * @param {number} azimuthalAngleDeg
-          * @param {GLSL.Float} polarAngleDeg
-          * @returns {GLSL.Vector3}
-          */
-         function getLightDirectionVector(azimuthalAngleDeg, polarAngleDeg) {
-            const polar = polarAngleDeg.radians();
-            const azimuthal = new GLSL.Float(azimuthalAngleDeg).radians();
-
-            return new GLSL.Vector3([
-               polar.sin().multiplyFloat(azimuthal.cos()),
-               polar.sin().multiplyFloat(azimuthal.sin()),
-               polar.cos(),
-            ]).normalize();
-         }
-
-         const originLightDirection = getLightDirectionVector(
-            originAzimuthalAngleDeg,
-            lightPolarAngleDeg
-         );
-         const orthogonalLightDirection = getLightDirectionVector(
-            orthogonalAzimuthalAngleDeg,
-            lightPolarAngleDeg
-         );
-         const oppositeLightDirection = getLightDirectionVector(
-            oppositeAzimuthalAngleDeg,
-            lightPolarAngleDeg
-         );
-
-         const lightMatrix = new GLSL.Matrix3([
-            [
-               originLightDirection.channel(0),
-               originLightDirection.channel(1),
-               originLightDirection.channel(2),
-            ],
-            [
-               orthogonalLightDirection.channel(0),
-               orthogonalLightDirection.channel(1),
-               orthogonalLightDirection.channel(2),
-            ],
-            [
-               oppositeLightDirection.channel(0),
-               oppositeLightDirection.channel(1),
-               oppositeLightDirection.channel(2),
-            ],
-         ]).inverse();
-
-         const reflection = new GLSL.Vector3([
-            originLuminance,
-            orthogonalLuminance,
-            oppositeLuminance,
-         ]);
-
-         return lightMatrix
-            .multiplyVector3(reflection)
-            .normalize()
-            .addFloat(new GLSL.Float(1))
-            .divideFloat(new GLSL.Float(2));
-      }
-
-      /** @type {number[][]} */
-      const anisotropicCombinations = [
-         [180, 270, 0],
-         [180, 90, 0],
-         [90, 180, 270],
-         [90, 0, 270],
-         [225, 315, 45],
-         [225, 135, 45],
-         [315, 45, 135],
-         [315, 225, 135],
-      ];
-
-      /** @type {GLSL.Vector3[]} */
-      const normalVectors = [];
-
-      anisotropicCombinations.forEach((combination) => {
-         /**
-          * @param {number} azimuthalAngle
-          * @returns {GLSL.Float}
-          */
-         function getLightLuminance(azimuthalAngle) {
-            const lightAzimuthalAngles = [0, 45, 90, 135, 180, 225, 270, 315];
-            const id = lightAzimuthalAngles.findIndex((value) => {
-               return value === azimuthalAngle;
-            });
-
-            return lightLuminances[id];
-         }
-
-         normalVectors.push(
-            getAnisotropicNormalVector(
-               getLightLuminance(combination[0]),
-               getLightLuminance(combination[1]),
-               getLightLuminance(combination[2]),
-               combination[0],
-               combination[1],
-               combination[2]
-            )
-         );
-      });
-
-      let normalVector = new GLSL.Vector3([
-         new GLSL.Float(0),
-         new GLSL.Float(0),
-         new GLSL.Float(0),
-      ])
-         .addVector3(...normalVectors)
-         .divideFloat(new GLSL.Float(normalVectors.length))
-         .normalize()
-         .multiplyFloat(mask);
-
-      if (cameraVerticalShift) {
-         // TODO: use cameraVerticalShift
-         /*const cameraAngle = Math.atan(
-            1 / Math.tan(lightPolarAngleDeg * (Math.PI / 180))
-         );
-
-         const zero = new GLSL.Float(0);
-         const one = new GLSL.Float(1);
-         const sine = new GLSL.Float(Math.sin(cameraAngle));
-         const cosine = new GLSL.Float(Math.cos(cameraAngle));
-
-         const rotationMatrix = new GLSL.Matrix3([
-            [one, zero, zero],
-            [zero, cosine, sine],
-            [zero, sine.multiplyFloat(new GLSL.Float(-1)), cosine],
-         ]);
-
-         normalVector = rotationMatrix.multiplyVector3(normalVector);*/
-      }
-
-      // TODO: fix alpha
-      const alpha = normalVector
-         .channel(0)
-         .minimum(normalVector.channel(1), normalVector.channel(2))
-         .multiplyFloat(new GLSL.Float(99999))
-         .minimum(new GLSL.Float(1));
-
-      this.outVariable = normalVector.getVector4(alpha);
-
-      this.shader.unbind();
-
-      this.initialized = true;
-   }
-}
