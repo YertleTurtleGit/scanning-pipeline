1e8df9fb5d03084db56d65da05d80f31250e4cf5
YertleTurtleGit <github@galzone.de>
2021-11-06 21:44:00 +0100

    nodes...

--- src/graph-node.js
+++ src/graph-node.js
@@ -3,15 +3,25 @@
 class GraphNode {
    /**
     * @param {Function} executer
-    * @param {{name:string, type:string}[]} inputs
-    * @param {{name:string, type:string}} output
+    * @param {{name:string, type:NODE_TYPE, description:string}[]} inputs
+    * @param {{name:string, type:NODE_TYPE, description:string}} output
+    * @param {string} displayName
     * @param {Function} thisParameter
     */
-   constructor(executer, inputs, output, thisParameter = null) {
+   constructor(
+      executer,
+      inputs,
+      output,
+      displayName = executer.name,
+      thisParameter = null
+   ) {
       this.executer = executer;
       this.inputs = inputs;
       this.output = output;
+      this.displayName = displayName;
       this.thisParameter = thisParameter;
+
+      this.nodeDiv = undefined;
    }
 
    /**
@@ -28,7 +38,52 @@ class GraphNode {
          });
       });
    }
+
+   /**
+    * @returns {HTMLDivElement} nodeDiv
+    */
+   getNodeDiv() {
+      if (this.nodeDiv) {
+         return this.nodeDiv;
+      }
+      this.nodeDiv = document.createElement("div");
+
+      const nodeTitleSpan = document.createElement("span");
+      nodeTitleSpan.innerText = this.displayName;
+      this.nodeDiv.appendChild(nodeTitleSpan);
+
+      const nodeDivInputList = document.createElement("ul");
+      this.nodeDiv.appendChild(nodeDivInputList);
+      const nodeDivOutputList = document.createElement("ul");
+      this.nodeDiv.appendChild(nodeDivOutputList);
+
+      this.inputs.forEach((input) => {
+         const inputListItem = document.createElement("li");
+         inputListItem.innerText = input.name + input.type;
+         nodeDivInputList.appendChild(inputListItem);
+      });
+
+      const outputListItem = document.createElement("li");
+      outputListItem.innerText = this.output.name + this.output.type;
+      nodeDivOutputList.appendChild(outputListItem);
+
+      this.nodeDiv.style.width = "15rem";
+      this.nodeDiv.style.padding = "0.5rem";
+      this.nodeDiv.style.border = "solid 0.1rem black";
+      this.nodeDiv.style.borderRadius = "0.5rem";
+
+      return this.nodeDiv;
+   }
 }
+
+/**
+ * @constant
+ * @typedef {string} NODE_TYPE
+ */
+GraphNode.NODE_TYPE = {
+   NUMBER: " ∈ ℝ",
+   IMAGE: " ⎚",
+};
 
 class GraphArea {
    /**
@@ -43,19 +98,15 @@ class GraphArea {
     * @param {GraphNode} graphNode
     */
    addNode(graphNode) {
-      this.div.appendChild(this.getNodeDiv(graphNode));
-   }
-
-   /**
-    * @private
-    * @param {GraphNode} graphNode
-    * @returns {HTMLDivElement} nodeDiv
-    */
-   getNodeDiv(graphNode) {
-      graphNode.inputs.forEach((input) => {});
+      this.div.appendChild(graphNode.getNodeDiv());
    }
 }
 
+/**
+ * @param {number} bla1
+ * @param {number} bla2
+ * @returns {number}
+ */
 function testFunction(bla1, bla2) {
    return bla1 + bla2;
 }
@@ -65,8 +116,10 @@ const graphArea = new GraphArea();
 const graphNode = new GraphNode(
    testFunction,
    [
-      { name: "bla1", type: typeof Number },
-      { name: "bla2", type: typeof Number },
+      { name: "bla1", type: GraphNode.NODE_TYPE.NUMBER },
+      { name: "bla2", type: GraphNode.NODE_TYPE.NUMBER },
    ],
-   { name: "resultbla", type: typeof Number }
+   { name: "resultbla", type: GraphNode.NODE_TYPE.NUMBER }
 );
+
+graphArea.addNode(graphNode);
