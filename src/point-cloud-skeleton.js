0f65794740304f450a81b2bed1bee86a12a8fb80
YertleTurtleGit <github@galzone.de>
2022-01-11 14:59:57 +0100

    ctn

--- src/point-cloud-skeleton.js
+++ src/point-cloud-skeleton.js
@@ -8,7 +8,9 @@ class PointCloudSkeleton {
     * @returns {{vertices:number[], colors:number[]}}
     */
    static pointCloudSkeleton(pointCloud) {
-      const downSampling = 0.75;
+      const MAX_ITERATIONS = 5;
+
+      const downSampling = 0.9;
 
       /** @type {{x:number, y:number, z:number}[]} */
       const vertices = [];
@@ -26,102 +28,142 @@ class PointCloudSkeleton {
          }
       }
 
-      console.log({ vertexCount: vertices.length });
-
-      const initialeNeighborhoodSize =
+      const initialNeighborhoodSize =
          PointCloudSkeleton.getInitialNeighborhoodSize(vertices);
-      console.log({ initialeNeighborhoodSize });
+      const initialMedianDrag = 0.5;
 
-      let medians = [];
-      let vertexIndexesToDelete = [];
-      let branchPointCandidates = [];
+      let neighborhoodSize = initialNeighborhoodSize;
+      let medianDrag = initialMedianDrag;
 
-      let neighborhoodSize = initialeNeighborhoodSize;
-      let medianDrag = initialeNeighborhoodSize;
+      let iteration = 0;
 
-      while (vertices.length > 0) {
+      while (iteration < MAX_ITERATIONS) {
+         medianDrag = Math.abs(medianDrag);
+         medianDrag = Math.min(medianDrag, 1);
+
          console.log({
             neighborhoodSize: neighborhoodSize,
             medianDrag: medianDrag,
             vertexCount: vertices.length,
+            iteration: iteration,
          });
+         iteration++;
 
-         medians = [];
-         vertexIndexesToDelete = [];
-         branchPointCandidates = [];
+         /** @type {{vertex:{x:number, y:number, z:number}, alignment:number, direction:{x:number, y:number, z:number}}[]} */
+         const branchPointCandidates = [];
+         let highestAlignmentVertex = {
+            vertex: {},
+            alignment: 0,
+            direction: {},
+         };
 
          vertices.forEach((vertexA, indexA) => {
-            /** @type {{x:number,y:number,z:number}[]} */
+            /** @type {{x:number, y:number, z:number}[]} */
             const neighbors = [];
 
             vertices.forEach((vertexB) => {
-               const distance = Math.sqrt(
-                  Math.pow(vertexA.x - vertexB.x, 2) +
-                     Math.pow(vertexA.y - vertexB.y, 2) +
-                     Math.pow(vertexA.z - vertexB.z, 2)
-               );
+               const isInNeighborhoodCube =
+                  vertexB.x > vertexA.x - neighborhoodSize &&
+                  vertexB.x < vertexA.x + neighborhoodSize &&
+                  vertexB.y > vertexA.y - neighborhoodSize &&
+                  vertexB.y < vertexA.y + neighborhoodSize &&
+                  vertexB.z > vertexA.z - neighborhoodSize &&
+                  vertexB.z < vertexA.z + neighborhoodSize;
 
-               if (distance < neighborhoodSize && distance > 0) {
-                  neighbors.push(vertexB);
+               if (isInNeighborhoodCube) {
+                  const distance = Math.sqrt(
+                     Math.pow(vertexA.x - vertexB.x, 2) +
+                        Math.pow(vertexA.y - vertexB.y, 2) +
+                        Math.pow(vertexA.z - vertexB.z, 2)
+                  );
+                  const isInNeighborhoodSphere = distance < neighborhoodSize;
+
+                  if (isInNeighborhoodSphere && distance > 0) {
+                     neighbors.push(vertexB);
+                  }
                }
             });
 
             if (neighbors.length > 0) {
-               const anisotropicDegree =
-                  PointCloudSkeleton.getAnisotropicDegree(neighbors);
-               if (anisotropicDegree > 0.9) {
-                  branchPointCandidates.push({
+               const PCAs = PointCloudSkeleton.getPCAs(neighbors);
+               const alignment = PCAs.alignment;
+               const direction = PCAs.direction;
+
+               if (alignment > 0.9) {
+                  const degreeVertex = {
                      vertex: vertexA,
-                     anisotropicDegree: anisotropicDegree,
-                  });
-                  vertexIndexesToDelete.push(indexA);
-               } else {
-                  medians.push(PointCloudSkeleton.getMedian(neighbors));
+                     alignment: alignment,
+                     direction: direction,
+                  };
+                  if (highestAlignmentVertex.alignment < alignment) {
+                     highestAlignmentVertex = degreeVertex;
+                  }
+                  branchPointCandidates.push(degreeVertex);
+               }
+
+               const median = PointCloudSkeleton.getMedian(neighbors);
+
+               const distanceToMedian = Math.sqrt(
+                  Math.pow(vertexA.x - median.x, 2) +
+                     Math.pow(vertexA.y - median.y, 2) +
+                     Math.pow(vertexA.z - median.z, 2)
+               );
+
+               const dragDirection = {
+                  x: (median.x - vertexA.x) / distanceToMedian,
+                  y: (median.y - vertexA.y) / distanceToMedian,
+                  z: (median.z - vertexA.z) / distanceToMedian,
+               };
+
+               vertices[indexA].x +=
+                  dragDirection.x * (medianDrag / distanceToMedian);
+               vertices[indexA].y +=
+                  dragDirection.y * (medianDrag / distanceToMedian);
+               vertices[indexA].z +=
+                  dragDirection.z * (medianDrag / distanceToMedian);
+
+               /** @type {{x:number, y:number, z:number}[]} */
+               const branchCandidates = [
+                  {
+                     x: highestAlignmentVertex.vertex.x,
+                     y: highestAlignmentVertex.vertex.y,
+                     z: highestAlignmentVertex.vertex.z,
+                  },
+               ];
+
+               branchPointCandidates.forEach((candidate) => {
+                  if (
+                     highestAlignmentVertex.vertex.x !== candidate.vertex.x ||
+                     highestAlignmentVertex.vertex.y !== candidate.vertex.y ||
+                     highestAlignmentVertex.vertex.z !== candidate.vertex.z
+                  ) {
+                     const angle = Math.cos(
+                        highestAlignmentVertex.vertex.x * candidate.vertex.x +
+                           highestAlignmentVertex.vertex.y *
+                              candidate.vertex.y +
+                           highestAlignmentVertex.vertex.z * candidate.vertex.z
+                     );
+                     if (angle < 1.5 && angle > -1.5) {
+                        branchCandidates.push({
+                           x: candidate.vertex.x,
+                           y: candidate.vertex.y,
+                           z: candidate.vertex.z,
+                        });
+                     }
+                  }
+               });
+
+               if (branchCandidates.length >= 5) {
+                  branchPoints.push(...branchCandidates);
                }
             }
          });
 
-         vertexIndexesToDelete.forEach((index) => {
-            vertices.splice(index);
-         });
-
-         if (medians.length > 0) {
-            vertices.forEach((vertex, index) => {
-               /** @type {{x:number, y:number, z:number}} */
-               let closestMedian;
-               let closestDistance = Number.MAX_VALUE;
-
-               medians.forEach((median) => {
-                  const distance = Math.sqrt(
-                     Math.pow(vertex.x - median.x, 2) +
-                        Math.pow(vertex.y - median.y, 2) +
-                        Math.pow(vertex.z - median.z, 2)
-                  );
-                  if (distance < closestDistance) {
-                     closestMedian = median;
-                     closestDistance = distance;
-                  }
-               });
-
-               if (closestMedian && closestDistance) {
-                  const dragDirection = {
-                     x: (closestMedian.x - vertex.x) / closestDistance,
-                     y: (closestMedian.y - vertex.y) / closestDistance,
-                     z: (closestMedian.z - vertex.z) / closestDistance,
-                  };
-
-                  vertices[index].x += dragDirection.x * medianDrag;
-                  vertices[index].y += dragDirection.y * medianDrag;
-                  vertices[index].z += dragDirection.z * medianDrag;
-               }
-            });
-         } else {
-            console.warn("no medians");
-         }
-
-         neighborhoodSize += neighborhoodSize / 2;
-         medianDrag += neighborhoodSize / 2;
+         neighborhoodSize += initialNeighborhoodSize / 2;
+         //medianDrag += initialMedianDrag / 2;
       }
+
+      console.log({ verticesLeft: vertices.length });
 
       const skeletonVertices = [];
       const skeletonColors = [];
@@ -130,7 +172,7 @@ class PointCloudSkeleton {
          skeletonColors.push(1, 0, 0);
       });
 
-      branchPointCandidates.forEach((vertex) => {
+      branchPoints.forEach((vertex) => {
          skeletonVertices.push(vertex.x, vertex.y, vertex.z);
          skeletonColors.push(0, 1, 0);
       });
@@ -141,7 +183,7 @@ class PointCloudSkeleton {
    /**
     * @private
     * @param {{x:number, y:number,z:number}[]} vertices
-    * @returns {{x:number, y:number,z:number}}
+    * @returns {{x:number, y:number, z:number}}
     */
    static getMedian(vertices) {
       // TODO Median not average.
@@ -161,21 +203,35 @@ class PointCloudSkeleton {
       return average;
    }
 
-   static getAnisotropicDegree(vertices) {
+   /**
+    * @private
+    * @param {{x:number, y:number, z:number}[]} vertices
+    * @returns {{alignment: number, direction:{x:number, y:number, z:number}}}
+    */
+   static getPCAs(vertices) {
       const verticesArray = [];
       vertices.forEach((vertex) => {
          verticesArray.push([vertex.x, vertex.y, vertex.z]);
       });
 
       const eigenvalues = [];
-      PCA.getEigenVectors(verticesArray).forEach((eigenVector) => {
+      // @ts-ignore
+      const eigenVectors = PCA.getEigenVectors(verticesArray);
+      eigenVectors.forEach((eigenVector) => {
          eigenvalues.push(eigenVector.eigenvalue);
       });
 
       const sum = eigenvalues[0] + eigenvalues[1] + eigenvalues[2];
-      if (sum === 0) return 0;
+      let alignment = 0;
+      if (sum !== 0) alignment = eigenvalues[0] / sum;
 
-      return eigenvalues[0] / sum;
+      const direction = {
+         x: (eigenVectors[0][0] + eigenVectors[1][0] + eigenVectors[2][0]) / 3,
+         y: (eigenVectors[0][1] + eigenVectors[1][1] + eigenVectors[2][1]) / 3,
+         z: (eigenVectors[0][2] + eigenVectors[1][2] + eigenVectors[2][2]) / 3,
+      };
+
+      return { alignment: alignment, direction: direction };
    }
 
    /**
